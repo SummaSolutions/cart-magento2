b7365e9539f1a990b1a6d06f9b8ebd2d1f25fe5f
imasson <imasson@summasolutions.net>
2016-06-27 12:10:00 -0300

    #668 - [BUG] If MercadoEnvíos is selected as a shipping method the only payment method available should be MercadoPago Classic

---
+++ app/code/MercadoPago/MercadoEnvios/Observer/FilterActivePaymentMethods.php
@@ -0,0 +1,48 @@
+<?php
+namespace MercadoPago\MercadoEnvios\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class FilterActivePaymentMethods
+    implements ObserverInterface
+{
+    /**
+     * @var \MercadoPago\MercadoEnvios\Helper\Data
+     */
+    protected $shipmentHelper;
+
+    protected $_useMercadoEnvios;
+
+
+    public function __construct(
+        \MercadoPago\MercadoEnvios\Helper\Data $shipmentHelper
+    )
+    {
+        $this->shipmentHelper = $shipmentHelper;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        if ($this->_useMercadoEnvios()) {
+            $event = $observer->getEvent();
+            $methodInstance = $event->getMethodInstance();
+            if (!$methodInstance instanceof \MercadoPago\Core\Model\Standard\Payment) {
+                $result = $observer->getEvent()->getResult();
+                $result->setData('is_available', false);
+            }
+        }
+        return $observer;
+    }
+
+    protected function _useMercadoEnvios()
+    {
+        if (empty($this->_useMercadoEnvios)) {
+            $quote = $this->shipmentHelper->getQuote();
+            $shippingMethod = $quote->getShippingAddress()->getShippingMethod();
+            $this->_useMercadoEnvios = $this->shipmentHelper->isMercadoEnviosMethod($shippingMethod);
+        }
+
+        return $this->_useMercadoEnvios;
+    }
+
+}
