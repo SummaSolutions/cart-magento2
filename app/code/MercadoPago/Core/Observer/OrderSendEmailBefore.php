cb3983cc1a300df39ae0658dc7a9e5da26159adb
imasson <imasson@summasolutions.net>
2017-01-23 11:31:29 -0300

    Deleted payment information from new order email details

---
+++ app/code/MercadoPago/Core/Observer/OrderSendEmailBefore.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MercadoPago\Core\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+
+class OrderSendEmailBefore
+    implements ObserverInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $transport = $observer->getTransport();
+        if ($transport->getOrder()->getPayment()->getMethod() == \MercadoPago\Core\Model\Custom\Payment::CODE ||
+            $transport->getOrder()->getPayment()->getMethod() == \MercadoPago\Core\Model\Standard\Payment::CODE ||
+            $transport->getOrder()->getPayment()->getMethod() == \MercadoPago\Core\Model\CustomTicket\Payment::CODE) {
+            $payment_html = preg_replace('#<(' . implode('|', ["tr"]) . ')(?:[^>]+)?>.*?</\1>#s', '', $transport->getPaymentHtml());
+            $transport->setPaymentHtml($payment_html);
+            $observer->setTransport($transport);
+        }
+    }
+}
