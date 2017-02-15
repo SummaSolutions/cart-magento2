85696432d3f85d57031868dd46bf30cc5bed37b7
Cristian Torres <ctorres@summasolutions.net>
2017-02-15 15:32:41 -0300

    re#868 - access token validation edited

--- app/code/MercadoPago/Core/Block/Calculator/CalculatorLink.php
+++ app/code/MercadoPago/Core/Block/Calculator/CalculatorLink.php
@@ -64,8 +64,8 @@ class CalculatorLink
     public function isAvailableCalculator(){
 
         $accessToken = $this->_scopeConfig->getValue(\MercadoPago\Core\Helper\Data::XML_PATH_ACCESS_TOKEN);
-        return  ($this->_helperData->isValidAccessToken($accessToken)
-                            & $this->_helperData->isAvailableCalculator());
+        $isValidAccessToken = $this->_helperData->isValidAccessToken($accessToken);
+        return  ($isValidAccessToken & !empty($this->_helperData->getPublicKey()) & $this->_helperData->isAvailableCalculator());
     }
 
     /**
