710a34c7e4d23683facd9ae1b934823140769b1f
Cristian Torres <ctorres@summasolutions.net>
2017-04-18 09:50:57 -0300

    re#943 - [T] Mercado Pago - Failure Page

--- src/MercadoPago/Core/Controller/Checkout/Page.php
+++ src/MercadoPago/Core/Controller/Checkout/Page.php
@@ -1,5 +1,6 @@
 <?php
-namespace MercadoPago\Core\Controller\Success;
+namespace MercadoPago\Core\Controller\Checkout;
+use MercadoPago\Core\Model\Core;
 
 
 /**
@@ -21,7 +22,7 @@ class Page
     protected $_orderFactory;
 
     /**
-     * @var OrderSender
+     * @var \Magento\Sales\Model\Order\Email\Sender\OrderSender
      */
     protected $_orderSender;
 
@@ -30,22 +31,41 @@ class Page
      */
     protected $_logger;
 
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $_scopeConfig;
 
     /**
-     * Page cosntructor
+     * @var \MercadoPago\Core\Helper\Data
+     */
+    protected $_helperData;
+
+    protected $_core;
+
+    /**
+     * Page constructor.
      *
+     * @param Core                                                $core
      * @param \Magento\Framework\App\Action\Context               $context
      * @param \Magento\Checkout\Model\Session                     $checkoutSession
      * @param \Magento\Sales\Model\OrderFactory                   $orderFactory
      * @param \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender
      * @param \Psr\Log\LoggerInterface                            $logger
+     * @param \MercadoPago\Core\Helper\Data                       $helperData
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface  $scopeConfig
+     * @param \MercadoPago\Core\Model\Core                        $core
      */
+
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Checkout\Model\Session $checkoutSession,
         \Magento\Sales\Model\OrderFactory $orderFactory,
         \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender,
-        \Psr\Log\LoggerInterface $logger
+        \Psr\Log\LoggerInterface $logger,
+        \MercadoPago\Core\Helper\Data $helperData,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \MercadoPago\Core\Model\Core $core
     )
     {
 
@@ -53,6 +73,9 @@ class Page
         $this->_orderFactory = $orderFactory;
         $this->_orderSender = $orderSender;
         $this->_logger = $logger;
+        $this->_helperData = $helperData;
+        $this->_scopeConfig = $scopeConfig;
+        $this->_core = $core;
 
         parent::__construct(
             $context
@@ -92,13 +115,40 @@ class Page
     public function execute()
     {
         $this->_sendNewOrderMail();
-        $checkoutTypeHandle = $this->getCheckoutHandle();
-        $this->_view->loadLayout(['default', $checkoutTypeHandle]);
-        $this->_eventManager->dispatch(
-            'checkout_onepage_controller_success_action',
-            ['order_ids' => [$this->_getOrder()->getId()]]
-        );
-        $this->_view->renderLayout();
+
+
+        if (!$this->_scopeConfig->getValue(\MercadoPago\Core\Helper\Data::XML_PATH_USE_SUCCESSPAGE_MP, \Magento\Store\Model\ScopeInterface::SCOPE_STORE)){
+
+            $order = $this->_getOrder();
+
+            $info_payment = $this->_core->getInfoPaymentByOrder($order->getIncrementId());
+
+            $status = null;
+
+            //checkout Custom Credit Card
+            if (!empty($info_payment['status']['value'])){
+                $status = $info_payment['status']['value'];
+                //$detail = $info_payment['status_detail']['value'];
+            }
+            //checkout redirect
+            if ($status == 'approved' || $status == 'pending'){
+                $this->_view->loadLayout(['default', 'checkout_onepage_success']);
+                $this->_view->renderLayout();
+            } else {
+                $this->_view->loadLayout(['default', 'checkout_onepage_failure']);
+                $this->_view->renderLayout();
+            }
+
+        } else{
+            $checkoutTypeHandle = $this->getCheckoutHandle();
+            $this->_view->loadLayout(['default', $checkoutTypeHandle]);
+            $this->_eventManager->dispatch(
+                'checkout_onepage_controller_success_action',
+                ['order_ids' => [$this->_getOrder()->getId()]]
+            );
+            $this->_view->renderLayout();
+        }
+
     }
 
     /**
